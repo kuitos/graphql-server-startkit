febae21910d83195ff14ba7975a4561242db8544
Kuitos <kuitos.lau@gmail.com>
2017-06-21 11:30:31 +0800

    update babel config

--- models/user/connector.js
+++ models/user/connector.js
@@ -4,23 +4,20 @@
  * @since 2017-06-12
  */
 
-import http from '../../utils/http';
 import qs from 'querystring';
+import http from '../../utils/http';
 
 export async function getUser(userId) {
 
-	const user = await http.get(`http://127.0.0.1:8080/users/${userId}`).then(res => res.data);
-	return user;
+	return await http.get(`http://127.0.0.1:8080/users/${userId}`).then(res => res.data);
 }
 
 export async function getUsers(queryParams, pageNum, pageSize) {
 
 	const params = qs.stringify({ ...queryParams, pageNum, pageSize });
-	const users = await http.get(`http://127.0.0.1:8080/users?${params}`).then(res => res.data);
-	return users;
+	return await http.get(`http://127.0.0.1:8080/users?${params}`).then(res => res.data);
 }
 
 export async function getUserMixNick(userId) {
-	const nick = await http.get(`http://127.0.0.1:8080/taobao/${userId}/nick/`).then(res => res.data);
-	return nick;
+	return await http.get(`http://127.0.0.1:8080/taobao/${userId}/nick/`).then(res => res.data);
 }
